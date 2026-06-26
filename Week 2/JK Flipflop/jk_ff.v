40e0cff625a7e151115511c3fb2bad512138912d
srii-m <sriinidhim10@gmail.com>
2026-06-26 11:29:10 +0530

    Create jk_ff.v

---
+++ Week 2/JK Flipflop/jk_ff.v
@@ -0,0 +1,27 @@
+module jk_ff(
+input j,
+input k,
+input clk,
+input rst,
+output reg q,
+output qb
+    );
+    always @(posedge clk) 
+    begin
+    if(!rst)
+    begin
+    q<=1'b0;
+    end
+    else
+    begin
+    case ({j,k})
+    2'b00:q<=q;
+    2'b01:q<=1'b0;
+    2'b10:q<=1'b1;
+    2'b11:q<=qb;
+    endcase
+    end
+    end
+    assign qb = ~q;
+endmodule
+
